7d0e414cfb2f6dea8a1f40a63843bee627d46aa9
TylwythTeg <garciarobertoiv@yahoo.com>
2017-10-22 21:16:58 -0700

    Added functionality to piano.js that matches fretboard.js (minus needing to build the notes). With the piano, you just pass the piano body element, the note elements are found, and you can use visualPiano.showNotes() to only show a specific set of notes

--- piano.js
+++ piano.js
@@ -1,1 +1,59 @@
+var scale = {
+    object: "scale",
+    root: "E",
+    notes: [
+        "E",
+        "F#",
+        "G",
+        "A",
+        "B",
+        "C#",
+        "D"
+    ]
+}
+
+
+
+
+var visualPiano = function(element) {
+    var that = this;
+    
+    this.element = element;
+    this.noteElements;
+    
+    function findNoteElements() {
+        var keys = that.element.children;
+        that.noteElements = [];
+        for (var i = 0; i < keys.length; i++) {
+            var key = keys[i];
+            that.noteElements.push(key.firstElementChild);
+        }
+    }
+    findNoteElements();
+    
+    
+    this.showNotes = function(notes) {
+        var elements = this.noteElements;
+        for (var i = 0; i < elements.length; i++) {
+            var note = elements[i].textContent;
+            
+            if (notes.includes(note)) {
+                elements[i].classList.remove("invisible");
+            } else {
+                elements[i].classList.add("invisible");
+            }
+        }
+    }
+}
+
+
+
+
+
+var pianoElement = document.getElementsByClassName("piano_body")[0];
+vp = new visualPiano(pianoElement);
+vp.showNotes(scale.notes);
+
+console.log(vp);
+
 
